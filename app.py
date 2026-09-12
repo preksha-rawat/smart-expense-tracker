ba1dc112bdc5f0eb6c70d8911014b553655ae220
preksha rawat <preksharawat14@gmail.com>
2026-09-12 13:16:40 +0530

    add expense dataset and basic data loading

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("hello, expense tracker !")
+# Import the Pandas library and give it a short nickname 'pd'
+import pandas as pd
+
+print("--- Reading Data with Pandas ---")
+
+# Tell Pandas to read your exact file and store it in a DataFrame variable named 'df'
+df = pd.read_csv("data/expense.csv")
+
+# Print the entire DataFrame to the screen
+print(df)
